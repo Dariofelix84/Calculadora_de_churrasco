1f41d44f82d731a8cdcb7349478e27e5aa8e4662
Dariofelix84 <dariofa69@gmail.com>
2024-06-14 14:39:22 -0300

    Mudança na lógica de cálculo de gramas e kilos

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ function calculate() {
     const criancas = parseInt(document.getElementById('criancas').value) || 0;
 
     const carneBovina = (homens * 500) + (mulheres * 300) + (criancas * 200);
-    const frango = (homens * 200) + (mulheres * 200) + (criancas * 100);
-    const linguica = (homens * 200) + (mulheres * 200) + (criancas * 200);
-    const refrigerante = (homens * 300) + (mulheres * 400) + (criancas * 200);
-    const cerveja = (homens * 800) + (mulheres * 500);
+    const carneBovinaFormatada = carneBovina < 1000 ? `${carneBovina}g` : `${carneBovina / 1000} kg`
 
-    document.getElementById('result').innerHTML = `
+    const frango = (homens * 200) + (mulheres * 200) + (criancas * 100);
+    const frangoFormatada = frango < 1000 ? `${frango}g` : `${frango / 1000} kg`
+
+    const linguica = (homens * 200) + (mulheres * 200) + (criancas * 200);
+    const linguicaFormatada = linguica < 1000 ? `${linguica}g` : `${linguica / 1000} kg`
+
+    const refrigerante = (homens * 300) + (mulheres * 400) + (criancas * 200);
+    const refrigeranteFormatado = refrigerante < 1000 ? `${refrigerante}ml` : `${refrigerante / 1000} litros` 
+
+    const cerveja = (homens * 800) + (mulheres * 500);
+    const cervejaFormatada = cerveja < 1000 ? `${cerveja}ml` : `${cerveja / 1000} litros`
+
+        document.getElementById('result').innerHTML = `
         <h3>Lista de Produtos</h3>
-        <span>Carne Bovina: <strong>${carneBovina}g</strong></span>
-        <span>Frango: <strong>${frango}g</strong></span>
-        <span>Linguiça: <strong>${linguica}g</strong></span>
-        <span>Refrigerante: <strong>${refrigerante}ml</strong></span>
-        <span>Cerveja: <strong>${cerveja}ml</strong></span>
+        <span>Carne Bovina: <strong>${carneBovinaFormatada}</strong></span>
+        <span>Frango: <strong>${frangoFormatada}</strong></span>
+        <span>Linguiça: <strong>${linguicaFormatada}</strong></span>
+        <span>Refrigerante: <strong>${refrigeranteFormatado}</strong></span>
+        <span>Cerveja: <strong>${cervejaFormatada}</strong></span>
     `;
 }
 
